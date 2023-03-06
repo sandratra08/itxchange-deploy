4a06b5296b5867284b2674af65be8e4b52ab7541
sandratra08 <45383481+sandratra08@users.noreply.github.com>
2023-03-06 16:50:06 +0300

    update 2

--- startup.ci.sh
+++ startup.ci.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+/opt/wait-for-it.sh postgresql://postgres:P46hmnusAHnDKZq8xvJq@containers-us-west-147.railway.app:5519/railway
 npm run migration:run
 npm run seed:run
 npm run start:prod > /dev/null 2>&1 &
